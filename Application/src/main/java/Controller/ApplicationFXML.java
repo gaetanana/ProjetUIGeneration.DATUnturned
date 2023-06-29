61fbf4ec1bea595d19136442ae7be3ccc3e385d8
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-06-29 22:30:12 +0200

    Fin de soirée

--- Application/src/main/java/Controller/ApplicationFXML.java
+++ Application/src/main/java/Controller/ApplicationFXML.java
@@ -1,15 +1,25 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 
 public class ApplicationFXML {
+
+
+    @FXML
+    private Button buttonAide;
 
     @FXML
     private Button buttonImport;
@@ -33,4 +43,25 @@ public class ApplicationFXML {
 
 
     }
+
+    @FXML
+    protected void onHelpButtonCick() {
+
+        try {
+            // Load the fxml file for the popup
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/HelpFXML.fxml"));
+            Parent popupRoot = fxmlLoader.load();
+
+            // Create a new stage for the popup
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+            popupStage.setScene(new Scene(popupRoot));
+
+            // Show the popup stage
+            popupStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

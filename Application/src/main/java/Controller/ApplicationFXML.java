2bbc05d3426ac2e282fb879e0b9c5da0501a42c9
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-06-29 21:27:40 +0200

    ça débute fort

--- Application/src/main/java/Controller/ApplicationFXML.java
+++ Application/src/main/java/Controller/ApplicationFXML.java
@@ -2,6 +2,7 @@ package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -10,12 +11,28 @@ import java.util.Map;
 import static Modele.modeleApplication.readDATFile;
 
 public class ApplicationFXML {
-    @FXML
-    private Button buttonImport;
 
     @FXML
-    protected void onImportButtonClick() {
+    private Button buttonImport;
+    @FXML
+    private Text nomFichier;
+
+    @FXML
+    protected void onImportButtonClick() throws Exception {
         System.out.println("click");
-        System.out.println(buttonImport.getText());
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+
+        File file = fileChooser.showOpenDialog(buttonImport.getScene().getWindow());
+        //System.out.println(readDATFile(file));
+        //Vérifier que le fichier est bien un .dat
+        if (!file.getName().endsWith(".dat")) {
+            nomFichier.setText("Erreur : Le fichier n'est pas un .dat");
+            return;
+        }
+        //Afficher le nom du fichier dans le textNomFichier
+        nomFichier.setText(file.getName());
+
+
     }
 }

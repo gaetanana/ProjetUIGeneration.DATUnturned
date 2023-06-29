7701a3a801e36d8bb79fe061dc07f5f0b1d83daf
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-06-29 22:19:31 +0200

    3 fonctions :
    1) Fonction qui permet de connaitre le nombre de craft d'un item
    2) Fonction qui permet de lire les informations de l'item sans le bluePrints
    3) Fonction qui permet de récupérer la partie BluePrint

--- Application/src/main/java/Modele/modeleApplication.java
+++ Application/src/main/java/Modele/modeleApplication.java
@@ -1,31 +1,85 @@
 package Modele;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class modeleApplication {
-    /**
-     * Cette fonction permet de lire un fichier .DAT
-     */
-    public static Map<String, String> readDATFile(File file) throws Exception {
+
+    public static Map<String, String> readDataWithOutBluePrints(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
         Map<String, String> data = new HashMap<>();
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.contains(" ")) {
-                String[] parts = line.split(" ", 2);
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            // Skip Blueprints section
+            if (line.startsWith("Blueprints")) {
+                break;
+            }
+
+            // Split the line into key-value pair and put into map
+            String[] parts = line.split(" ");
+            if (parts.length > 1) {
                 data.put(parts[0], parts[1]);
-            } else {
-                data.put(line, "");
             }
         }
-        scanner.close();
+
         return data;
     }
 
+    public static Map<String, String> readBlueprintsData(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
+        Map<String, String> data = new HashMap<>();
+        String line;
+        boolean isBlueprintsSection = false;
 
+        while ((line = reader.readLine()) != null) {
+            // Start reading from Blueprints section
+            if (line.startsWith("Blueprints")) {
+                isBlueprintsSection = true;
+                continue;
+            }
 
+            if (isBlueprintsSection) {
+                // Split the line into key-value pair and put into map
+                String[] parts = line.split(" ");
+                if (parts.length > 1) {
+                    data.put(parts[0], parts[1]);
+                }
+            }
+        }
+
+        return data;
+    }
+
+    /**
+     * Cette fonction permet de récupérer
+     */
+    public static int getNumberOfBlueprints(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+        String line;
+        Pattern pattern = Pattern.compile("Blueprints (\\d+)");
+
+        while ((line = reader.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+
+            if (matcher.find()) {
+                return Integer.parseInt(matcher.group(1));
+            }
+        }
+
+        throw new IllegalArgumentException("No blueprints line found in the file.");
+    }
 }
+
+
+
+
+
